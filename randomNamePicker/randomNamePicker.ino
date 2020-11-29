b1f5e0b5041b6f5dc7a8f70b9fc7fa01b40f8a2f
Martti Raavel <mrt@tlu.ee>
2020-11-29 16:01:37 +0200

    Font suuremaks

--- randomNamePicker/randomNamePicker.ino
+++ randomNamePicker/randomNamePicker.ino
@@ -1,24 +1,26 @@
 /////////////////////////////////////////////////////////////////
 #include "Button2.h"; //  https://github.com/LennartHennigs/Button2
 #include "ESPRotary.h";
-
+// Vajalikud teegid
 #include "ssd1306.h"
 #include "nano_gfx.h"
 #include "sova.h"
 
 /////////////////////////////////////////////////////////////////
 
+// Nupu ja vibroanduri viigud
 #define ROTARY_PIN1	D7
 #define ROTARY_PIN2	D6
 #define BUTTON_PIN  D8
 #define VIBRATION_PIN D5
 
-#define CLICKS_PER_STEP   4   // this number depends on your rotary encoder 
-
+#define CLICKS_PER_STEP 4 // this number depends on your rotary encoder 
 
 /////////////////////////////////////////////////////////////////
+// Päevikute menüü
 SAppMenu menu;
 
+// Massiiv päevikute menüü jaoks
 const char *subjects[] =
 {
     "Eesti keel",
@@ -26,23 +28,25 @@ const char *subjects[] =
     "Kunstiopetus"
 };
 
-
+// Enkoodri ja nupu (meil enkoodri küljes) definitsioonid
 ESPRotary r = ESPRotary(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP);
 Button2 b = Button2(BUTTON_PIN);
 
+// Esimese päeviku õpilaste nimekiri
 char *subject0[] = {
   "Quentin Good",
   "Zakariya Ireland",
   "Amrit Allen",
   "Lina Hail",
   "Louie Lu",
-  "Benedict Schofield",
+  "Benedict Schofi",
   "Nathaniel Kelley",
   "Naseem Rooney",
   "Leigh Clemons",
   "Safa Ray"
 };
 
+// Teise päeviku õpilaste nimekiri
 char *subject1[] = {
   "Quentin",
   "Zakariya",
@@ -56,6 +60,7 @@ char *subject1[] = {
   "Safa"
 };
 
+// Kolmanda päeviku õpilaste nimekiri
 char *subject2[] = {
   "Good",
   "Ireland",
@@ -69,29 +74,30 @@ char *subject2[] = {
   "Ray"
 };
 
+// Õpilaste nimkirjade suuruste arvutamine
 int subject0Count = sizeof(subject0) / sizeof(char *) - 1;
 int subject1Count = sizeof(subject1) / sizeof(char *) - 1;
 int subject2Count = sizeof(subject2) / sizeof(char *) - 1;
 
-int subjectsCount = sizeof(subjects) / sizeof(char *) - 1;
+// Päevikute menüü suuruse arvutamine
+int subjectsCount = sizeof(subjects) / sizeof(char *);
 
+// Mitu millisekundit näidatakse õpilase nime ekraanil
 int showNameDelay = 3000;
 
 /////////////////////////////////////////////////////////////////
 
 void setup() {
   pinMode(VIBRATION_PIN, INPUT_PULLUP);
-  Serial.begin(9600);
   delay(50);
   
   r.setLeftRotationHandler(updateCounter);
   r.setRightRotationHandler(updateCounter);
 
   b.setTapHandler(click);
-  b.setLongClickHandler(resetPosition);
 
   /* Select the font to use with menu and all font functions */
-  ssd1306_setFixedFont(ssd1306xled_font6x8);
+  ssd1306_setFixedFont(ssd1306xled_font8x16);
   sh1106_128x64_i2c_init();
   ssd1306_clearScreen( );
   ssd1306_createMenu( &menu, subjects, subjectsCount );
@@ -108,35 +114,34 @@ void loop() {
 }
 
 /////////////////////////////////////////////////////////////////
+// Nime kuvamise funktsioon - nime kuvatakse erinevast massiivist vastavalt menüü valikule
 void showName()
 {
-    // Serial.println(students[random(0, studentsCount)]);
-    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_setFixedFont(ssd1306xled_font8x16);
     ssd1306_clearScreen();
 
     switch (ssd1306_menuSelection(&menu))
     {
-        case 0:
-            ssd1306_printFixed(0,  8, subject0[random(0, subject0Count)] , STYLE_NORMAL);
-            break;
-
-        case 1:
-            ssd1306_printFixed(0,  8, subject1[random(0, subject1Count)] , STYLE_NORMAL);
-            break;
-
-        case 2:
-            ssd1306_printFixed(0,  8, subject2[random(0, subject2Count)] , STYLE_NORMAL);
-            break;
-            
-        default:
-            break;
+      case 0:
+        ssd1306_printFixed(0, 16, subject0[random(0, subject0Count)] , STYLE_NORMAL);
+        break;
+      case 1:
+        ssd1306_printFixed(0, 16, subject1[random(0, subject1Count)] , STYLE_NORMAL);
+        break;
+      case 2:
+        ssd1306_printFixed(0, 16, subject2[random(0, subject2Count)] , STYLE_NORMAL);
+        break;   
+      default:
+        break;
     }
+    // Nime ekraanil kuvamise paus
     delay(showNameDelay);
+    
     ssd1306_clearScreen();
     ssd1306_showMenu(&menu);
 }
 
-// on left or right rotation
+// Nupu keeramise registreerimine ja menüüs valiku muutmine
 void updateCounter(ESPRotary& r) {
   if(r.getDirection() > 100)
   {
@@ -150,17 +155,9 @@ void updateCounter(ESPRotary& r) {
 }
 
 
-// single click
+// Nupu vajutuse registreerimine
 void click(Button2& btn) {
-  Serial.println("Click!");
-  Serial.println(ssd1306_menuSelection(&menu));
   showName();
-}
-
-// long click
-void resetPosition(Button2& btn) {
-  r.resetPosition();
-  Serial.println("Reset!");
 }
 
 
